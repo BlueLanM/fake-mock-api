3f775e7914f2bb9c8ae570a921a66f1b9d8e1739
wxt <wb.wangxiaoteng01@mesg.corp.netease.com>
2025-12-23 11:13:40 +0800

    update:path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/json-test/",
+	base: "/fake-mock-api/",
 	// GitHub Pages 需要设置为仓库名
 	build: {
 		outDir: "docs"
